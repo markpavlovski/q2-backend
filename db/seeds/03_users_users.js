c7b7a7e2c032447dd9766ceb969886a672bfab43
Mark Pavlovski <markpavlovski@gmail.com>
2018-04-23 16:43:32 -0700

    added users_users seed

---
+++ db/seeds/03_users_users.js
@@ -0,0 +1,17 @@
+const TABLE_NAME = 'users_users'
+
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex(TABLE_NAME).del()
+    .then(function () {
+      // Inserts seed entries
+      return knex(TABLE_NAME).insert([
+        {id: 1, users_id: 1, friends_id: 2},
+        {id: 2, users_id: 2, friends_id: 1}
+      ])
+    })
+    .then(() => {
+      // reset sequence
+      return knex.raw(`SELECT setval('${TABLE_NAME}_id_seq', (SELECT MAX(id) FROM ${TABLE_NAME}));`)
+    })
+}
