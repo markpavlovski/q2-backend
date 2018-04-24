4f67c1e9d29da7b4931b65d9303bfa13ff4e577d
Mark Pavlovski <markpavlovski@gmail.com>
2018-04-24 14:56:44 -0700

    added user ids

--- db/seeds/02_messages.js
+++ db/seeds/02_messages.js
@@ -8,11 +8,16 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex(TABLE_NAME).insert([
-        {id: 1, users_id: 1, location: st.geography(st.geomFromText('Point(47.614445 -122.322622)', 4326)), message: 'hello'},
+        {id: 1, users_id: 1, location: st.geography(st.geomFromText('Point(47.620361 -122.349947)', 4326)), message: 'space needle'},
+        {id: 2, users_id: 1, location: st.geography(st.geomFromText('Point(47.592506 -122.334418)', 4326)), message: 'safeco field'},
+        {id: 3, users_id: 2, location: st.geography(st.geomFromText('Point(47.608089 -122.341192)', 4326)), message: 'pike place'},
+        {id: 4, users_id: 2, location: st.geography(st.geomFromText('Point(47.650852 -122.347812)', 4326)), message: 'fremont troll'}
         // {id: 2, users_id: 1, location: '47.614445, -122.322622', message: 'world'},
         // {id: 3, users_id: 2, location: '47.614445, -122.322622', message: 'woof'},
-        // {id: 4, users_id: 2, location: '47.614445, -122.322622', message: 'woof woof'}
+        // {id: 4, users_id: 2, location: '47.614445, -122.322622', message: 'woof woof'}47.620361, -122.349947
       ])
+
+
     })
     .then(() => {
       // reset sequence
--- src/models/messages.js
+++ src/models/messages.js
@@ -4,7 +4,7 @@ console.log(st);
 const bcrypt = require('bcrypt-as-promised')
 const userModel = require('./users')
 const location = '47.614445, -122.322622'
-const currentLocation = '47.614440, -122.322620'
+const currentLocation = '47.598886, -122.333791' // galvanize
 // const currentLocation = '0, 0'
 
 
@@ -20,18 +20,95 @@ function create(message,id){
     )
   })
 }
+//
+// function distance(distance){
+//   console.log(distance);
+//   let friends = db('users_users').where('users_id', 1)
+//   return friends
+//   .then(()=> return (
+//     db.raw(`SELECT *
+//         FROM messages
+//         where ST_DWithin(messages.location, ST_MakePoint(${currentLocation})::geography, ${distance})`)
+//   ))
+//   .then(messages=>messages.filter(message.))
+// }
+//
+//
+// function distance(distance){
+//   console.log(distance);
+//   return (
+//     // db('messages')
+//     // // .where({location})
+//     // .returning('*')
+//     db.raw(`SELECT *
+//         FROM messages
+//         where ST_DWithin(messages.location, ST_MakePoint(${currentLocation})::geography, ${distance})`)
+//   )
+// }
 
-function distance(distance){
-  console.log(distance);
-  return (
-    // db('messages')
-    // // .where({location})
-    // .returning('*')
-    db.raw(`SELECT *
-        FROM messages
-        where ST_DWithin(messages.location, ST_MakePoint(${currentLocation})::geography, 1000)`)
-  )
+
+
+
+function distance(distance,onlyFriends=false,onlyMine=true){
+  if (onlyFriends) {
+    let localMessages
+    let myFriends
+    return (
+      db.raw(`SELECT *
+          FROM messages
+          where ST_DWithin(messages.location, ST_MakePoint(${currentLocation})::geography, ${distance})`)
+    )
+    .then(messages => {
+      localMessages = messages.rows
+      // console.log(localMessages.filter(message => message.id == 1));
+      return db('users_users').where('users_id', 1)
+    })
+    .then(friends => {
+      myFriends = friends
+      return localMessages.filter(message => friends.some(friend => message.users_id === friend.friends_id))
+    })
+  } else if (onlyMine) {
+    let localMessages
+    let myFriends
+    return (
+      db.raw(`SELECT *
+          FROM messages
+          where ST_DWithin(messages.location, ST_MakePoint(${currentLocation})::geography, ${distance})`)
+    )
+    .then(messages => {
+      return messages.rows.filter(message => message.users_id == 1)
+    })
+  } else {
+    return (
+      db.raw(`SELECT *
+          FROM messages
+          where ST_DWithin(messages.location, ST_MakePoint(${currentLocation})::geography, ${distance})`)
+    )
+  }
 }
+
+//
+// function distance(distance,id=1){
+//   console.log(distance)
+//   let mymessages
+//   console.log(db.raw(`SELECT * FROM messages
+//       ST_DWithin(messages.location, ST_MakePoint(${currentLocation})::geography, ${distance})`));
+//   return (
+//     db.raw(`SELECT * FROM messages
+//         ST_DWithin(messages.location, ST_MakePoint(${currentLocation})::geography, ${distance})`)
+//   )
+//   .then((messages)=> {
+//     mymessages = messages
+//     return db('users_users').where('users_id', id)
+//   })
+//   .then(friends => {
+//     return mymessages.filter((message) => {
+//       return friends.some(friend => friend.friends_id === message.user_id )
+//     })
+//   })
+// }
+
+
 
 module.exports = {
   create,
